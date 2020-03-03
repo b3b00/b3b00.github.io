2e3647236a1486f75ea3ba60985b1b060333df12
Olivier Duhart <olivier.duhart@sage.com>
2020-03-03 17:39:19 +0100

    use localstorage for store

--- store.js
+++ store.js
@@ -1,5 +1,30 @@
 import { writable } from 'svelte/store';
 
-export const count = writable(0);
+// export const count = writable(0);
 
-export const switched = writable(true);
+// export const switched = writable(true);
+
+
+const createWritableStore = (key, startValue) => {
+    const { subscribe, set, update } = writable(startValue);
+    
+      return {
+      subscribe,
+      update,
+      set,
+      useLocalStorage: () => {
+        const json = localStorage.getItem(key);
+        if (json) {
+          set(JSON.parse(json));
+        }
+        
+        subscribe(current => {
+          localStorage.setItem(key, JSON.stringify(current));
+        });
+      }
+    };
+  }
+  
+  export const count = createWritableStore('count', 0);
+
+  export const switched = createWritableStore('switched',false);
