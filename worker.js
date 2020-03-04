d1f6a56bd119de555fdd40a11ce8aaedd6326fc2
Olivier Duhart <olivier.duhart@sage.com>
2020-03-04 14:44:40 +0100

    .

--- worker.js
+++ worker.js
@@ -1,5 +1,4 @@
-
-const pwaCache = "pwa-cache"
+const pwaCache = "pwa-cache";
 
 console.log("starting worker");
 
@@ -15,57 +14,53 @@ const assets = [
   "/images/icons/icon-152x152.jpg",
   "/images/icons/icon-192x192.jpg",
   "/images/icons/icon-384x384.jpg",
-  "/images/icons/icon-512x512.jpg"  
-]
+  "/images/icons/icon-512x512.jpg"
+];
 
-self.addEventListener('activate', function(event) {
-  console.log('Claiming control');
+self.addEventListener("activate", function(event) {
+  console.log("Claiming control");
   return self.clients.claim();
-  });
+});
 
-  // self.addEventListener('install', event => {
-  //   console.log('Service worker installed.');
-  // });
+// self.addEventListener('install', event => {
+//   console.log('Service worker installed.');
+// });
 
-console.log('register install event');
-self.addEventListener('install', installEvent => {
-
+console.log("register install event");
+self.addEventListener("install", installEvent => {
   try {
-  console.log("install event called",assets);
-  installEvent.waitUntil(
-    caches.open(pwaCache).then(cache => {
-      console.log("caching ",assets);
-      cache.addAll(assets)
-    })
-  );
-  console.log("worker is installed now.");
+    console.log("install event called", assets);
+    installEvent.waitUntil(
+      caches.open(pwaCache).then(cache => {
+        console.log("caching ", assets);
+        cache.addAll(assets);
+      })
+    );
+    console.log("worker is installed now.");
+  } catch (e) {
+    console.log("error", e);
   }
-  catch(e) {
-    console.log("error",e);
-  }
-})
+});
 
-self.addEventListener('fetch', event => {
-  console.log('Fetching:', event.request.url);
+self.addEventListener("fetch", event => {
+  console.log("Fetching:", event.request.url);
 });
 
 self.addEventListener("fetch", fetchEvent => {
-  console.log("worker fetching",fetchEvent)
+  console.log("worker fetching", fetchEvent);
   try {
     fetchEvent.respondWith(
       caches.match(fetchEvent.request).then(res => {
         try {
-        return res || fetch(fetchEvent.request)
-        }
-        catch(e) {
-          console.log("feth",e);
+          return res || fetch(fetchEvent.request);
+        } catch (e) {
+          console.log("feth", e);
         }
       })
     );
+  } catch (e) {
+    console.log("error fetch event : ", event, e);
+  }
+});
 
-    catch(e) {
-      console.log("error fetch event : ",event,e);
-    }
-  });
-
-  // console.log("all callbacks registered");
+// console.log("all callbacks registered");
