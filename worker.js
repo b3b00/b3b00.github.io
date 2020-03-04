dcd19e24360f2f04a727508f88808d440d5547cf
Olivier Duhart <olivier.duhart@sage.com>
2020-03-04 09:08:16 +0100

    worker

--- worker.js
+++ worker.js
@@ -1,8 +1,36 @@
 
+const pwaCache = "pwa-cache"
+
 console.log("starting worker");
 
+const assets = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/dist/bundle.js",
+  "/images/icons/icon-72x72.png",
+  "/images/icons/icon-96x96.jpg",
+  "/images/icons/icon-128x128.jpg",
+  "/images/icons/icon-144x144.jpg",
+  "/images/icons/icon-152x152.jpg",
+  "/images/icons/icon-192x192.jpg",
+  "/images/icons/icon-384x384.jpg",
+  "/images/icons/icon-512x512.jpg"  
+]
 
+self.addEventListener('activate', function(event) {
+  console.log('Claiming control');
+  return self.clients.claim();
+  });
+
+console.log('register install event');
 self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(pwaCache).then(cache => {
+      console.log("caching ",assets);
+      cache.addAll(assets)
+    })
+  )
   console.log("worker is installed now.");
 })
 
